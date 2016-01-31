cfa9c72336671c6c3937a1bbf11bfd94f57bae1e
calledT <huanggengtao@gmail.com>
2016-02-01 00:16:53 +0800

    init

---
+++ README.md
@@ -0,0 +1,69 @@
+# Social Share
+
+## 特性
+* 使用iconfont当图标，容易自定义样式
+* 样式打包在脚本里，减少http请求
+* 支持初始化的时候传参或者使用`data-`属性传参数，后者优先级高
+* PC端支持IE7以上浏览器,除了[qrcode](https://github.com/davidshimjs/qrcodejs)外，无其他库依赖
+* 移动端支持微信、QQ、微博的原生应用分享(借用UC浏览器或者QQ浏览器进行)
+* 移动端gzip之后仅5kb,PC端也仅10kb
+
+### PC端(sosh.js)
+
+#### 参数说明
+
+1. `Sosh`构造函数第一个参数为字符串，代表selector，支持id和类名两种形式，如`#id`或者`.classname`
+2. `Sosh`构造函数第二个参数对象字面量，代表分享组件初始化的配置
+    - `url` 分享的地址，默认值为网页当前的地址 
+    - `title` 分享的标题，默认值为当前网页`title`元素的内容
+    - `digest` 分享的描述，默认值为当前网页`<meta name="description">`元素的content
+    - `pic` 分享的图片，默认值为当前网页第一个`img`元素的图片地址
+    - `sites` 页面上显示分享的网站，为数组类型，默认值为`['weixin', 'weibo', 'yixin', 'qzone']`
+
+#### 使用说明
+插入以下js引用到body标签结束前，默认初始化拥有类名`sosh`的元素
+```html
+<script src="dist/sosh.js"></script>
+```
+
+初始化的时候传参
+```javascript
+new Sosh('.sosh', {title: 'socail share', sites: ['weixin', 'weibo', 'yixin']})
+```
+
+也支持使用dataset进行配置,dataset配置优先级比函数参数高
+```html
+<div class="sosh" data-title="social share" data-site='weixin,weibo,yixin'></div>
+```
+
+
+### 移动端(msosh.js)
+
+#### 参数说明
+1. `Msosh`构造函数第一个参数为字符串，代表selector，支持`querySelectorAll`所支持的参数类型
+2. `Msosh`构造函数第二个参数对象字面量，代表分享组件初始化的配置
+    - `url` 分享的地址，默认值为网页当前的地址 
+    - `title` 分享的标题，默认值为当前网页`title`元素的内容
+    - `digest` 分享的描述，默认值为当前网页`<meta name="description">`元素的content
+    - `pic` 分享的图片，默认值为当前网页第一个`img`元素的图片地址
+    - `sites` 页面上显示分享的网站，为数组类型，默认值为`[weixin', 'weixintimeline', 'yixin', 'weibo', 'qq', 'qzone]`
+    
+#### 使用说明
+插入以下js引用到body标签结束前
+```html
+<script src="dist/msosh.js"></script>
+```
+然后使用构造函数进行初始化,同样支持使用dataset进行配置
+```javascript
+var share = new Msosh('.msosh', {title: 'socail share', sites: ['weixin', 'weixintimeline', 'qq', 'qzone', 'weibo']);
+```
+在初始化之后，可以使用`share.popIn()`函数弹出分享网站列表浮层，调用这个函数的时候依然可以配置分享的参数和显示的网站，但配置仅在第一次调用的时候生效。
+
+在UC浏览器和QQ浏览器里支持唤起微信、QQ、微博客户端进行分享。其他浏览器里支持唤起QQ客户端的分享，微博分享使用webapi进行，而微信分享需要借用QQ浏览器进行，如果用户没有安装，则点击无反应。
+
+## 样式
+因为CSS打包在js里所以无需另外引用，但是由于图标使用iconfont，所以还需要把图标放在网页能请求到的地方，当然也可以自定义优先级更高的样式覆盖默认的样式。
+
+## License
+
+MIT © calledT
---
+++ gulpfile.js
@@ -0,0 +1,146 @@
+'use strict'
+
+var $               = require('gulp-load-plugins')();
+var _               = require('lodash');
+var fs              = require('fs');
+var del             = require('del');
+var gulp            = require('gulp');
+var path            = require('path');
+var merge           = require('merge-stream');
+var runSequence     = require('run-sequence');
+var browserSync     = require('browser-sync');
+var autoprefixer    = require('autoprefixer');
+var pkg             = require('./package.json');
+
+var SRC_PATH         = 'src';
+var ICONPREFIX       = 'icon';
+var ICONFONTNAME     = 'soshfont';
+var ICONFONTTEMPLATE = 'foundation-style';
+
+// STATS
+var SRC = _.mapKeys(pkg.src, function(value, key) {
+  value.globext = path.join('*' + value.ext);
+  value.globpath = path.join(value.dir, '**', value.globext);
+  return key;
+});
+
+gulp.task('sass', function() {
+  var processors = [autoprefixer({browsers: pkg.browsers})];
+
+	var stream = gulp.src(SRC.scss.globpath)
+			.pipe($.sourcemaps.init())
+			.pipe($.sass({
+				includePaths: [SRC.scss.dir],
+				precision: 8
+			}).on('error', $.sass.logError))
+			.pipe($.postcss(processors))
+			.pipe($.sourcemaps.write('./maps'))
+			.pipe(gulp.dest(SRC.css.dir))
+			.pipe($.if(SRC.css.globext, browserSync.stream()));
+
+	return stream;
+});
+
+gulp.task('package', function() {
+  var jsStream = gulp.src(['src/js/qrcode.js', 'src/js/sosh.js']);
+  var cssStream = gulp.src('src/css/sosh.css')
+      .pipe($.cssnano({compatibility: 'ie7'}))
+      .pipe($.cssToJs());
+
+  var stream = merge(jsStream, cssStream)
+      .pipe($.concat('sosh.js'))
+      .pipe($.uglify())
+      .pipe(gulp.dest('dist'));
+
+  return stream;
+});
+
+gulp.task('packagem', function() {
+  var jsStream = gulp.src('src/js/msosh.js');
+  var cssStream = gulp.src('src/css/msosh.css')
+      .pipe($.cssnano())
+      .pipe($.cssToJs());
+
+  var stream = merge(jsStream, cssStream)
+      .pipe($.concat('msosh.js'))
+      .pipe($.uglify())
+      .pipe(gulp.dest('dist'));
+
+  return stream;
+});
+
+gulp.task('iconfont', function() {
+  var stream = gulp.src(SRC.iconsvg.globpath)
+      .pipe($.imagemin({
+        svgoPlugins: [{transformsWithOnePath: true}]
+      }))
+      .pipe($.iconfont({
+        fontName: ICONFONTNAME,
+        appendUnicode: true,
+        formats: ['ttf', 'eot', 'woff', 'woff2', 'svg'],
+        timestamp: 0
+      }))
+      .on('glyphs', function(glyphs) {
+        var options = {
+          glyphs: glyphs.map(function(glyph) {
+            return {name: glyph.name, codepoint: glyph.unicode[0].charCodeAt(0)}
+          }),
+          fontName: ICONFONTNAME,
+          fontPath: '../iconfont/',
+          className: ICONPREFIX
+        };
+
+        // 创建iconfont scss模块
+        var cssTmplPath = path.join(SRC.iconsvg.dir, 'template', ICONFONTTEMPLATE + '.css');
+        var htmlTmplPath = path.join(SRC.iconsvg.dir, 'template', ICONFONTTEMPLATE + '.html');
+        gulp.src(cssTmplPath)
+          .pipe($.consolidate('lodash', options))
+          .pipe($.rename({basename: '_' + ICONFONTNAME, extname: '.scss'}))
+          .pipe(gulp.dest(path.join(SRC.scss.dir, 'component')));
+
+        // 创建示例页面
+        gulp.src(cssTmplPath)
+          .pipe($.consolidate('lodash', options))
+          .pipe($.rename({basename: ICONFONTNAME}))
+          .pipe(gulp.dest('src/sample'));
+
+        gulp.src(htmlTmplPath)
+          .pipe($.consolidate('lodash', options))
+          .pipe($.rename({basename:'sample'}))
+          .pipe(gulp.dest('src/sample'));
+      })
+      .pipe(gulp.dest(SRC.iconfont.dir));
+
+  return stream;
+});
+
+gulp.task('move', function() {
+  var iconfontStream = gulp.src(SRC.iconfont.globpath, {base: 'src'})
+      .pipe(gulp.dest('dist'));
+
+  var imgStream = gulp.src(SRC.img.globpath, {base: 'src'})
+      .pipe($.imagemin())
+      .pipe(gulp.dest('dist'));
+
+  return merge(iconfontStream, imgStream);
+})
+
+gulp.task('clean', function(cb) {
+  del('dist', cb());
+})
+
+gulp.task('serve', ['iconfont'], function(cb) {
+	browserSync.init({
+		open: 'external',
+    startPath: 'html',
+		server: {baseDir: ['src', 'dist']}
+	});
+
+	gulp.watch(SRC.scss.globpath, ['sass']);
+  gulp.watch(SRC.js.globpath, browserSync.reload);
+	gulp.watch(SRC.html.globpath, browserSync.reload);
+});
+
+gulp.task('build', function(cb) {
+  runSequence(['iconfont', 'sass', 'clean'], ['package', 'packagem', 'move'], cb);
+});
